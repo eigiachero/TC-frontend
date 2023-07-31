7708dd86fd4455d95ed4676d64e14946f1a4371a
Eze Giachero <eigiachero@gmail.com>
2023-07-31 19:19:13 -0300

    Initialize React Proyect with Vite and Shadcn UI

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+
+function App() {
+  return (
+    <div className="flex flex-col place-items-center p-4 font-sans ">
+      <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl m-4">
+        Tasks board
+      </h1>
+      <Button>Simple button</Button>
+    </div>
+  )
+}
+
+export default App
